b6e5aa2f3a7ee232e3467bd835f6ee3d3deca95e
Shannon <22020995@myrp.edu.sg>
2023-05-25 13:54:23 +0800

    Layout and Menus added

--- app/src/main/java/sg/edu/rp/c346/id22020995/mylocalbanks/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22020995/mylocalbanks/MainActivity.java
@@ -3,12 +3,29 @@ package sg.edu.rp.c346.id22020995.mylocalbanks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDBS;
+    TextView tvOCBC;
+    TextView tvUOB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDBS = findViewById(R.id.textViewDBS);
+        tvOCBC = findViewById(R.id.textViewOCBC);
+        tvUOB = findViewById(R.id.textViewUOB);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.language_menu, menu);
+        return true;
     }
 }
